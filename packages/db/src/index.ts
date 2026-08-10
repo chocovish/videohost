2bfdefcf7a167b59b76828b8547cb921368d9297
chocovish <vishal.ghosh28@gmail.com>
2026-08-10 17:02:43 +0530

    feat: removing prisma query log in dev mode

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -24,7 +24,7 @@ export const db =
   globalThis.prisma ||
   new PrismaClient({
     adapter,
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
